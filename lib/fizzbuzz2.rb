9f04e84d9c907667fcf47eff6829ef60c468b4ee
smarbaf <smarbaf@hotmail.com>
2015-06-14 17:34:47 +0100

    Refactored to a single hash

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,15 +1,14 @@
 def fizzbuzz(number)
 
-  if number % 15 == 0
-    return "FizzBuzz"
-  elsif number % 3 == 0
-    return 'Fizz'
-  elsif number % 5 == 0
-    return 'Buzz'
-  else
-    return number
 
-  end
+  hash = {"FizzBuzz" => number % 3 == 0 && number % 5 == 0,
+          "Fizz" => number % 3 == 0 && number % 5 != 0,
+          "Buzz" => number % 5 == 0 && number % 3 != 0
+          }
 
+  hash.key(true) || number
 
 end
+
+puts fizzbuzz(3), fizzbuzz(5), fizzbuzz(15)
+
